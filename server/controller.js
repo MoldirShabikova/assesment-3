d4abf3e042e755efc939afdf70d3fb176dc74a6f
moldir <moldirshb@gmail.com>
2022-12-02 16:03:49 -0600

    adding part 1

--- server/controller.js
+++ server/controller.js
@@ -8,6 +8,20 @@ module.exports = {
         let randomCompliment = compliments[randomIndex];
       
         res.status(200).send(randomCompliment);
+    },
+
+    getFortune: (req, res) => {
+        const fortunes = ['Failure is the chance to do better next time.', 'A fresh start will put you on your way.', 'A new perspective will come with the new year.', 'A pleasant surprise is waiting for you', 'A truly rich life contains love and art in abundance'];
+      
+        // choose random compliment
+        let randomIndex = Math.floor(Math.random() * fortunes.length);
+        let randomFortune = fortunes[randomIndex];
+      
+        res.status(200).send(randomFortune);
     }
+
+    
+
+
 
 }
